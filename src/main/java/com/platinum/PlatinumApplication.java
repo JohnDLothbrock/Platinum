94828cc23c77d92c4ae9f4f1c6dd64746f19c93d
JohnDLothbrock <an.ju.yo@outlook.com>
2025-11-03 12:16:29 -0600

    Initial commit

---
+++ src/main/java/com/platinum/PlatinumApplication.java
@@ -0,0 +1,13 @@
+package com.platinum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatinumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatinumApplication.class, args);
+	}
+
+}
