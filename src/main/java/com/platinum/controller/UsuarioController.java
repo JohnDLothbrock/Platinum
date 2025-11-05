56e2ad3ff83c396f4fcc10dc56b1b1a8b7383430
JohnDLothbrock <an.ju.yo@outlook.com>
2025-11-05 16:49:02 -0600

    controller estaba mal colocado
    controller estaba mal colocado

--- src/main/java/com/platinum/controller/UsuarioController.java
+++ src/main/java/com/platinum/controller/UsuarioController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controller;
+package com.platinum.controller;
 
 import org.springframework.ui.Model;
 import com.platinum.service.UsuarioService;
