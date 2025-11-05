5d23fa2367641f209cbcb731637e2d6ceae5d073
JohnDLothbrock <an.ju.yo@outlook.com>
2025-11-05 14:05:05 -0600

    creacion del controller
    controller para usuario

---
+++ src/main/java/controller/UsuarioController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import org.springframework.ui.Model;
+import com.platinum.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/listado")
+    public String inicio(Model model) {
+
+        var usuarios = usuarioService.getUsuarios(false);
+        model.addAttribute("usuarios", usuarios);
+        model.addAttribute("totalUsuarios", usuarios.size());
+        return "usuario/listado";
+    }
+
+}
