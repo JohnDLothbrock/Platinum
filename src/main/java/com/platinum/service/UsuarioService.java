26644330ee06383a6ae98221b238c6d3fb9cdc6d
JohnDLothbrock <an.ju.yo@outlook.com>
2025-11-05 14:55:56 -0600

    cambios en index, html

--- src/main/java/com/platinum/service/UsuarioService.java
+++ src/main/java/com/platinum/service/UsuarioService.java
@@ -11,6 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.platinum.domain.Usuario;
+import com.platinum.repository.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class UsuarioService {
 
